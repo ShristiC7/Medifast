8db7143fb2c174a3132215a07b8e8ff7165dc7d7
Ayush Ahirwar <78347950+Ayusha94751@users.noreply.github.com>
2025-09-23 10:39:38 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key in .env");
+}
+
+createRoot(document.getElementById("root")!).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <App />
+  </ClerkProvider>
+);
